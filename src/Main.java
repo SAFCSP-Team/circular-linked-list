c3e88c0e252f58f82634f5a4d55773def9a4cecf
lamabeta <148013077+lamabeta@users.noreply.github.com>
2024-01-07 13:44:06 +0300

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+   public static void main(String[] args) {  
+       CreateList cl = new CreateList();  
+       cl.add(1);  
+       cl.add(2);  
+       cl.add(3);  
+       cl.add(4);  
+       cl.display();
+    }
+}
