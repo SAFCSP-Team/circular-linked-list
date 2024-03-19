53946d485faac2c290b507e1896c8663ad52b699
JSQdev <148073798+devJSQ@users.noreply.github.com>
2024-03-19 17:04:57 +0300

    Update CreateList.java

--- src/CreateList.java
+++ src/CreateList.java
@@ -2,5 +2,28 @@ public class CreateList {
    public Node head = null;  
    public Node tail = null;  
      
-    /* your code here */
+    public void add(int data){
+        Node newNode = new Node (data);
+        if (head == null){
+            head = newNode;
+            tail = newNode;
+        } else{
+            newNode.next = head;
+            head = newNode;
+            tail = newNode;
+        }
+    }
+
+    public void display(){
+        if (head != null){
+            Node current = head;
+            do {
+                System.out.println(current.data);
+                current = current.next;
+            } while(current != head);
+            System.out.println();
+        } else{
+            System.out.println("list is empty");
+        }
+    }
 }
